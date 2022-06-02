a56cfbc64dbe75d88ed7ad3be41333c9fb9e761c
Tahmid <tahmid@inflexionpointbd.com>
2022-06-02 17:09:28 +0600

    Middleware system added, authentication added and admin panel layout added

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,3 +10,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// -------------------------------------------------------------- Admin Panel Content ----------------------------------------------------------------------------
+
+
+Route::middleware('auth')->group(function () {
+
+
+// -------------------------------- Dashboard --------------------------------
+
+
+    Route::get('admin/dashboard', "AdminController@index")->name('admin.dashboard');
+
+// -------------------------------- Dashboard --------------------------------
+
+
+// --------------------------------- Log out ---------------------------------
+
+    Route::get('logout', "AdminController@logout")->name('logout');
+
+// --------------------------------- Log out ---------------------------------
+});
+
+
+// -------------------------------------------------------------- Admin Panel Content ----------------------------------------------------------------------------
+
