c5c60681a67bcdb0794899d52ec9da588773f0ca
sumaiyaa-t <sumaiya@insurecow.com>
2022-06-08 00:10:00 +0600

    Add internship

---
+++ resources/views/user/internship.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7 ltie8 ltie9" lang="en-US"><![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 ltie9" lang="en-US"><![endif]-->
+<!--[if !(IE 7) | !(IE 8)]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="initial-scale=1.0"/>
+
+    <title>Arannayk - Internship</title>
+
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Lato%3A100%2C100italic%2C300%2C300italic%2Cregular%2Citalic%2C700%2C700italic%2C900%2C900italic&amp;subset=latin&amp;"
+          type="text/css" media="all"/>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Noto+Sans%3Aregular%2Citalic%2C700%2C700italic&amp;subset=greek%2Ccyrillic-ext%2Ccyrillic%2Clatin%2Clatin-ext%2Cvietnamese%2Cgreek-ext&amp;"
+          type="text/css" media="all"/>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Merriweather%3A300%2C300italic%2Cregular%2Citalic%2C700%2C700italic%2C900%2C900italic&amp;subset=latin%2Clatin-ext&amp;"
+          type="text/css" media="all"/>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Mystery+Quest%3Aregular&amp;subset=latin%2Clatin-ext&amp;"
+          type="text/css" media="all"/>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('plugins/superfish/css/superfish.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('plugins/dl-menu/component.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('plugins/elegant-font/style.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('plugins/fancybox/jquery.fancybox.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('plugins/flexslider/flexslider.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('css/style-responsive.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('css/style-custom.css') }}" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('plugins/masterslider/public/assets/css/masterslider.main.css') }}"
+          type="text/css"
+          media="all"/>
+    <link rel="stylesheet" href="{{ asset('css/master-custom.css') }}" type="text/css" media="all"/>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+          integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+</head>
+
+<body data-rsssl="1"
+      class="home page-template-default page page-id-5680 _masterslider _msp_version_3.2.7 woocommerce-no-js">
+<div class="body-wrapper float-menu" data-home="https://demo.goodlayers.com/greennature/">
+
+    <!-------------------------------Navigation Starts Here---------------------------------------------------------->
+
+    <header class="greennature-header-wrapper header-style-5-wrapper greennature-header-with-top-bar">
+
+        <!-------------------------------------------------------------------------
+          ------------------------------------------------------------------------
+          Top navigation---------------------------------------------------------
+        ---------------------------------------------------------------------- -->
+
+        <x-user.navbar.top-navigation-component></x-user.navbar.top-navigation-component>
+
+        <!---------------------------------------------------------------------------------
+          --------------------------------------------------------------------------------
+        ---------------------------------------------------- Top navigation------------------ -->
+
+
+        <div id="greennature-header-substitute"></div>
+
+        <div class="greennature-header-inner header-inner-header-style-5">
+
+            <div class="greennature-header-container container">
+                <div class="greennature-header-inner-overlay"></div>
+
+                <!-----------------------------------------------------------------
+                  -----------------------------------------------------------------
+                  -----------Mobile Navbar-----------------------------------------
+                  ------------------------------------------------------------------>
+
+                <x-user.navbar.mobile-navbar-component></x-user.navbar.mobile-navbar-component>
+
+
+                <!-----------------------------------------------------------------
+                  -----------------------------------------------------------------
+                  -----------------------------------------------------------------
+                  -----------------------------Mobile Navbar----------------------->
+                <x-user.navbar.pc-navbar-component></x-user.navbar.pc-navbar-component>
+
+                <!-----------------------------------------------------------------
+                  -----------------------------------------------------------------
+                  ------------------- PC Navbar -----------------------------------
+                  ---------------------------------------------------------------->
+
+
+                <!-----------------------------------------------------------------
+                  -----------------------------------------------------------------
+                  ----------------------------------------------------------------
+                  --------------------------------------------PC Navbar-------------------->
+                <div class="clear"></div>
+            </div>
+        </div>
+    </header>
+    <!------------------------------------------ Navbar ends here--- -->
+
+
+    <div class="greennature-page-title-wrapper header-style-5-title-wrapper">
+        <div class="greennature-page-title-overlay"></div>
+        <div class="greennature-page-title-container container">
+            <h1 class="section-heading-title">Get Involved</h1>
+            <span class="greennature-page-caption">Internship</span>
+        </div>
+    </div>
+
+    <section>
+        <div class="content-wrapper">
+            <div class="greennature-content">
+
+                <!-- Above Sidebar Section-->
+                <div class="above-sidebar-wrapper">
+                    <section id="content-section-1">
+                        <div class="section-container container">
+
+                            @foreach($careers as $career)
+                                <div class="eight columns">
+                                    <div class="greennature-item greennature-accordion-item style-1" style="margin-bottom: 60px;">
+                                        <div class="accordion-tab active pre-active">
+                                            <h4 class="accordion-title"><i class="icon-minus" ></i><span>{{ $career->title }}</span></h4>
+                                            <div class="accordion-content">
+                                                <p>{!! $career->description !!} </p>
+                                                <p>Deadline: {{ $career->deadline }}</p>
+                                                <a class="greennature-button small" href="mailto: abc@example.com" target="_self" style="color:#ffffff; background-color:#f38142; ">Apply</a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            @endforeach
+
+                            <div class="four columns">
+                                <div class="greennature-item-title-wrapper greennature-item  greennature-left greennature-small ">
+                                    <div class="greennature-item-title-container container">
+                                        <div class="greennature-item-title-head">
+                                            <h3 class="greennature-item-title greennature-skin-title greennature-skin-border">Let's stay in touch.</h3>
+                                            <div class="clear"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="greennature-item greennature-content-item">
+                                    <div role="form" class="wpcf7" id="wpcf7-f4-o1" lang="en-US" dir="ltr">
+                                        <div class="screen-reader-response"></div>
+                                        <form action="" method="post" class="wpcf7-form" novalidate="novalidate">
+
+                                            <p>Your Name (required)
+                                                <br />
+                                                <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" /></span> </p>
+                                            <p>Your Email (required)
+                                                <br />
+                                                <span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" /></span> </p>
+                                            <p>Subject
+                                                <br />
+                                                <span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span> </p>
+                                            <p>Your Message
+                                                <br />
+                                                <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false"></textarea></span> </p>
+                                            <p>
+                                                <input type="submit" value="Send" class="wpcf7-form-control wpcf7-submit" />
+                                            </p>
+                                            <div class="wpcf7-response-output wpcf7-display-none"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </section>
+                </div>
+
+                <!-- Sidebar With Content Section-->
+
+                <!-- Below Sidebar Section-->
+
+            </div>
+            <!-- greennature-content -->
+            <div class="clear"></div>
+        </div>
+    </section>
+
+
+    <!-------------------------------------------------------------------------
+  ------------------------------------------------------------------------
+  Footer------------------------------------------------------------------
+---------------------------------------------------------------------- -->
+
+    <x-user.footer.footer-component></x-user.footer.footer-component>
+
+    <!-------------------------------------------------------------------------
+        ------------------------------------------------------------------------
+        -----------------------------------------------------------------------
+      --------------------------------------------------Footer---------------- -->
+
+</div>
+<!-- body-wrapper -->
+
+<script type="text/javascript" src="{{ asset('js/jquery/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery/jquery-migrate.min.js') }}"></script>
+<script>
+    var ms_grabbing_curosr =
+            "plugins/masterslider/public/assets/css/common/grabbing.html",
+        ms_grab_curosr =
+            "plugins/masterslider/public/assets/css/common/grab.html";
+</script>
+<script type="text/javascript" src="{{ asset('plugins/superfish/js/superfish.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/hoverIntent.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/dl-menu/modernizr.custom.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/dl-menu/jquery.dlmenu.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/jquery.easing.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/fancybox/jquery.fancybox.pack.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/fancybox/helpers/jquery.fancybox-media.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/fancybox/helpers/jquery.fancybox-thumbs.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/flexslider/jquery.flexslider.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/jquery.isotope.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/plugins.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/masterslider/public/assets/js/masterslider.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/jquery.transit.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/gdlr-portfolio/gdlr-portfolio-script.js') }}"></script>
+
+</body>
+
+</html>
